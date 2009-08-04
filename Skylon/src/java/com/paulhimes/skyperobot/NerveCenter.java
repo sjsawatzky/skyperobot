29a44900f83dd9b4b7591cd0644eb18ebf528159
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-08-04 02:15:30 +0000

    Initial code commit

---
+++ Skylon/src/java/com/paulhimes/skyperobot/NerveCenter.java
@@ -0,0 +1,109 @@
+package com.paulhimes.skyperobot;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.List;
+
+public class NerveCenter {
+
+	public NerveCenter() {
+		// Instantiate the ChatListener
+		ChatListener chatListener = new ChatListener();
+
+		// Add the actions.
+		// chatListener.add(new SimpleChatAction() {
+		// public void received(ChatMessage message) throws SkypeException {
+		// if (message.getContent().contains("(wave)")) {
+		// message.getChat().send("(yes)");
+		// }
+		// }
+		// });
+
+		try {
+			Class[] classes = getClasses("com.paulhimes.skyperobot.chatactions");
+
+			for (Class clazz : classes) {
+				try {
+					ChatAction action = (ChatAction) Class.forName(clazz.getName()).newInstance();
+
+					chatListener.add(action);
+
+				} catch (InstantiationException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalAccessException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	/**
+	 * Scans all classes accessible from the context class loader which belong to the given package
+	 * and subpackages.
+	 * 
+	 * @param packageName
+	 *            The base package
+	 * @return The classes
+	 * @throws ClassNotFoundException
+	 * @throws IOException
+	 */
+	private static Class[] getClasses(String packageName) throws ClassNotFoundException,
+			IOException {
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		assert classLoader != null;
+		String path = packageName.replace('.', '/');
+		Enumeration<URL> resources = classLoader.getResources(path);
+		List<File> dirs = new ArrayList<File>();
+		while (resources.hasMoreElements()) {
+			URL resource = resources.nextElement();
+			dirs.add(new File(resource.getFile()));
+		}
+		ArrayList<Class> classes = new ArrayList<Class>();
+		for (File directory : dirs) {
+			classes.addAll(findClasses(directory, packageName));
+		}
+		return classes.toArray(new Class[classes.size()]);
+	}
+
+	/**
+	 * Recursive method used to find all classes in a given directory and subdirs.
+	 * 
+	 * @param directory
+	 *            The base directory
+	 * @param packageName
+	 *            The package name for classes found inside the base directory
+	 * @return The classes
+	 * @throws ClassNotFoundException
+	 */
+	private static List<Class> findClasses(File directory, String packageName)
+			throws ClassNotFoundException {
+		List<Class> classes = new ArrayList<Class>();
+		if (!directory.exists()) {
+			return classes;
+		}
+		File[] files = directory.listFiles();
+		for (File file : files) {
+			if (file.isDirectory()) {
+				assert !file.getName().contains(".");
+				classes.addAll(findClasses(file, packageName + "." + file.getName()));
+			} else if (file.getName().endsWith(".class")) {
+				classes.add(Class.forName(packageName + '.'
+						+ file.getName().substring(0, file.getName().length() - 6)));
+			}
+		}
+		return classes;
+	}
+
+}
---
+++ Skylon/src/java/com/paulhimes/skyperobot/chatactions/SimpleChatAction.java
@@ -0,0 +1,24 @@
+package com.paulhimes.skyperobot.chatactions;
+
+import com.paulhimes.skyperobot.ChatAction;
+import com.skype.ChatMessage;
+import com.skype.SkypeException;
+
+public class SimpleChatAction implements ChatAction {
+
+	@Override
+	public void received(ChatMessage message) throws SkypeException {
+		if (message.getContent().contains("(wave)")) {
+			message.getChat().send("(yes)");
+		}
+
+		System.out.println("hello");
+	}
+
+	@Override
+	public void sent(ChatMessage message) throws SkypeException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
