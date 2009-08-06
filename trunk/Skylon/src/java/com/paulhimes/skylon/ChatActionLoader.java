0f996a744249d58a010558f8b8d25f133f70dc82
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-08-06 05:09:56 +0000

    Added sender checking and action name to SimpleReplyChatAction.

--- trunk/Skylon/src/java/com/paulhimes/skylon/ChatActionLoader.java
+++ trunk/Skylon/src/java/com/paulhimes/skylon/ChatActionLoader.java
@@ -1,6 +1,7 @@
 package com.paulhimes.skylon;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.paulhimes.skylon.chatactions.SimpleReplyChatAction;
 
@@ -13,8 +14,9 @@ public final class ChatActionLoader {
 	public static List<ChatAction> loadActions() {
 		// Load all the actions
 		List<ChatAction> actions = new ArrayList<ChatAction>();
-		actions.add(new SimpleReplyChatAction(RegexBuilder.anything(),
-											  RegexBuilder.endsWith("\\(wave\\)"),
+		actions.add(new SimpleReplyChatAction("Wave Back", 
+											  RegexBuilder.anything(), 
+											  RegexBuilder.endsWith("\\(wave\\)"), 
 											  "(wave)"));
 
 		return actions;
