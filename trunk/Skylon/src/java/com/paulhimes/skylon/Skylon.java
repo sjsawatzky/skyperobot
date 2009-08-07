f9f9acd0a8ec1167fe48053dd0c7e4d10882a54a
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-08-07 03:36:26 +0000

    Started work on system tray notification balloon framework.

--- trunk/Skylon/src/java/com/paulhimes/skylon/Skylon.java
+++ trunk/Skylon/src/java/com/paulhimes/skylon/Skylon.java
@@ -1,7 +1,15 @@
 package com.paulhimes.skylon;
 
-import java.awt.*;
-import java.awt.event.*;
+import java.awt.AWTException;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 
 import javax.imageio.ImageIO;
@@ -14,7 +22,7 @@ public class Skylon {
 
 		trayIcon = createTrayIcon();
 
-		NerveCenter nerveCenter = new NerveCenter();
+		NerveCenter nerveCenter = new NerveCenter(trayIcon);
 		nerveCenter.addChatActions(ChatActionLoader.loadActions());
 	}
 
@@ -23,7 +31,8 @@ public class Skylon {
 			// get the SystemTray instance
 			SystemTray tray = SystemTray.getSystemTray();
 
-			TrayIcon trayIcon = new TrayIcon(createTrayImage(), "Skylon", createTrayMenu());
+			TrayIcon trayIcon = new TrayIcon(createTrayImage(), "Skylon",
+					createTrayMenu());
 
 			try {
 				tray.add(trayIcon);
