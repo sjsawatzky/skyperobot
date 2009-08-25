3c529431eefd68ef569178199a8d1ae0852b8f2c
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-08-25 01:02:16 +0000

    chat actions refactor

---
+++ trunk/Skylon/src/java/com/paulhimes/skylon/chatactions/ChatAction.java
@@ -0,0 +1,18 @@
+package com.paulhimes.skylon.chatactions;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+
+import com.paulhimes.skylon.ChatListener;
+import com.skype.ChatMessage;
+import com.skype.SkypeException;
+
+public interface ChatAction {
+	public void received(ChatMessage message) throws SkypeException;
+
+	public void sent(ChatMessage message) throws SkypeException;
+
+	public void registerCallback(ChatListener chatListener);
+
+	public Node encodeXml(Document document);
+}
