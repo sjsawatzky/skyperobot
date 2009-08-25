369bdbbab8eccbfb23328c123b25ad3f399e0f81
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-08-25 04:18:55 +0000

    Hard coded the reading of a single simplereplychataction from an xml file.

--- Skylon/src/java/com/paulhimes/skylon/chatactions/ChatActionWriter.java
+++ Skylon/src/java/com/paulhimes/skylon/chatactions/ChatActionWriter.java
@@ -1,5 +1,8 @@
 package com.paulhimes.skylon.chatactions;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
@@ -15,16 +18,14 @@ import com.sun.org.apache.xml.internal.serialize.XMLSerializer;
 
 public class ChatActionWriter {
 
-	public static Document document;
-
 	private ChatActionWriter() {
 
 	}
 
-	public static void writeActions(List<ChatAction> chatActions)
+	public static void writeActions(List<ChatAction> chatActions, File file)
 			throws ParserConfigurationException {
-		document = DocumentBuilderFactory.newInstance().newDocumentBuilder()
-				.newDocument();
+		Document document = DocumentBuilderFactory.newInstance()
+				.newDocumentBuilder().newDocument();
 
 		Node actionsNode = XmlTools.appendChild(document, "actions", document);
 		Node chatActionsNode = XmlTools.appendChild(actionsNode, "chatactions",
@@ -35,9 +36,14 @@ public class ChatActionWriter {
 		}
 
 		try {
-			XMLSerializer output = new XMLSerializer(System.out,
-					new OutputFormat(document));
+			FileOutputStream out = new FileOutputStream(file);
+			XMLSerializer output = new XMLSerializer(out, new OutputFormat(
+					document));
 			output.serialize(document);
+			out.close();
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		} catch (IOException e) {
 			System.err.println(e);
 		}
