2ff7b92afb40fdc848364ffa98b7644513e3f277
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-08-26 03:19:28 +0000

    removed duplicate model loading

--- trunk/Skylon/src/java/com/paulhimes/skylon/chatactions/ChatActionReader.java
+++ trunk/Skylon/src/java/com/paulhimes/skylon/chatactions/ChatActionReader.java
@@ -13,12 +13,6 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import com.paulhimes.skylon.chatactions.rules.Rule;
-import com.paulhimes.skylon.chatactions.rules.Rules;
-import com.paulhimes.skylon.chatactions.rules.Rule.RuleMatch;
-import com.paulhimes.skylon.chatactions.rules.Rule.RuleType;
-import com.paulhimes.skylon.chatactions.rules.Rules.RulesOperator;
-
 public final class ChatActionReader {
 
 	private ChatActionReader() {
@@ -28,56 +22,17 @@ public final class ChatActionReader {
 	public static List<ChatAction> readActions(File file) {
 
 		try {
-			Document document = DocumentBuilderFactory.newInstance()
-					.newDocumentBuilder().parse(file);
+			Document document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(file);
 
 			Element rootNode = document.getDocumentElement();
 			System.out.println(rootNode.getNodeName());
 
-			NodeList chatActionsNodes = rootNode
-					.getElementsByTagName("chatactions");
+			NodeList chatActionsNodes = rootNode.getElementsByTagName("chatactions");
 
 			Element chatActionsNode = (Element) chatActionsNodes.item(0);
-			NodeList simpleChatActionNodes = chatActionsNode
-					.getElementsByTagName("simplereplychataction");
 
-			Element simpleChatActionNode = (Element) simpleChatActionNodes
-					.item(0);
+			return SimpleReplyChatAction.parseXml(chatActionsNode);
 
-			System.out.println(simpleChatActionNode.getAttribute("name"));
-
-			NodeList replyNodes = simpleChatActionNode
-					.getElementsByTagName("reply");
-			Element replyNode = (Element) replyNodes.item(0);
-
-			NodeList rulesNodes = simpleChatActionNode
-					.getElementsByTagName("rules");
-			Element rulesNode = (Element) rulesNodes.item(0);
-
-			String operatorString = rulesNode.getAttribute("operator");
-			RulesOperator operator = RulesOperator.valueOf(operatorString);
-
-			List<Rule> rulesList = new ArrayList<Rule>();
-
-			NodeList ruleNodes = rulesNode.getElementsByTagName("rule");
-			for (int i = 0; i < ruleNodes.getLength(); i++) {
-				Element rule = (Element) ruleNodes.item(i);
-				RuleMatch match = RuleMatch.valueOf(rule.getAttribute("match"));
-				boolean negativeFlag = Boolean.parseBoolean(rule
-						.getAttribute("negativeFlag"));
-				RuleType type = RuleType.valueOf(rule.getAttribute("type"));
-				String content = rule.getTextContent();
-				rulesList.add(new Rule(type, match, content, negativeFlag));
-			}
-
-			SimpleReplyChatAction chatAction = new SimpleReplyChatAction(
-					simpleChatActionNode.getAttribute("name"), replyNode
-							.getTextContent(), new Rules(operator, rulesList));
-
-			List<ChatAction> actions = new ArrayList<ChatAction>();
-			actions.add(chatAction);
-
-			return actions;
 		} catch (SAXException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -90,18 +45,5 @@ public final class ChatActionReader {
 		}
 
 		return new ArrayList<ChatAction>();
-		// // Load all the actions
-		// List<ChatAction> actions = new ArrayList<ChatAction>();
-		//
-		// List<Rule> rulesList = new ArrayList<Rule>();
-		// rulesList.add(new Rule(RuleType.SENDER, RuleMatch.CONTAINS, "echo",
-		// false));
-		// rulesList.add(new Rule(RuleType.CONTENT, RuleMatch.ENDS_WITH,
-		// "\\(wave\\)", false));
-		// Rules rules = new Rules(RulesOperator.AND, rulesList);
-		// actions.add(new SimpleReplyChatAction("Wave Back", "Hello World!",
-		// rules));
-		//
-		// return actions;
 	}
 }
