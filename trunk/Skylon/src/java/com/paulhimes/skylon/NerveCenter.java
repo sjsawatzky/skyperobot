63c0e80a00175667f455eedb3dabf87d6d431fbe
paulhimes <paulhimes@7b417c9c-7fe3-11de-8668-3f6947b3de95>
2009-09-04 03:00:51 +0000

    Initial work on edit actions table.

--- trunk/Skylon/src/java/com/paulhimes/skylon/NerveCenter.java
+++ trunk/Skylon/src/java/com/paulhimes/skylon/NerveCenter.java
@@ -3,28 +3,31 @@ package com.paulhimes.skylon;
 import java.awt.TrayIcon;
 import java.awt.TrayIcon.MessageType;
 
-import com.paulhimes.skylon.chatactions.ChatActions;
 import com.paulhimes.skylon.logging.Logger;
 
 public final class NerveCenter {
 
 	private final Logger logger = new Logger(getClass());
 
+	private final Actions actions;
 	private final ChatListener chatListener;
 	private final TrayIcon trayIcon;
 
-	public NerveCenter(TrayIcon trayIcon) {
+	public NerveCenter(TrayIcon trayIcon, Actions actions) {
 		// Instantiate the ChatListener
 		chatListener = new ChatListener(this);
 		this.trayIcon = trayIcon;
-		giveMessage("By your command!");
-	}
+		this.actions = actions;
 
-	public void addChatActions(ChatActions actions) {
-		chatListener.setChatActions(actions);
+		chatListener.setChatActions(actions.getChatActions());
+		giveMessage("By your command!");
 	}
 
 	public void giveMessage(String message) {
 		trayIcon.displayMessage("Event Triggered", message, MessageType.INFO);
+	}
+
+	public Actions getActions() {
+		return actions;
 	}
 }
